5d4ec966300d5a29b6d7b014a49119698b2dd37f
Koronis.lua <rileyfeldman757@gmail.com>
2026-02-18 10:02:59 +1100

    Update loader-93439052235418240563-1771369333953.lua

--- loader-93439052235418240563-1771369333953.lua
+++ loader-93439052235418240563-1771369333953.lua
@@ -1,19 +1,7 @@
 
---[[
+--[[       
 
-You can upload this file on your github, pastebin, anywhere you want and generate a loadstring.
-    
-Add 'lp_key' variable if your user has a key on top of this loader.
-
-You can remove this message block as you like.
-
-
- _                ____            _   
-| |   _   _  __ _|  _ \ _ __ ___ | |_ 
-| |  | | | |/ _` | |_) | '__/ _ \| __|
-| |__| |_| | (_| |  __/| | | (_) | |_ 
-|_____\__,_|\__,_|_|   |_|  \___/ \__|
-                                    
+nice one buddy your not slick
 
 ]]--
 
